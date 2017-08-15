4730138c2bafb42466e79a4f00b9079caa916ae0
parthdesai97 <parthdesai@Parths-MBP.attlocal.net>
2017-08-14 22:08:11 -0700

    First

---
+++ skill-sample-nodejs-trivia-master/src/questions.js
@@ -0,0 +1,546 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "Who is the only men\'s tennis player to reach world number one of the ATP singles rankings without winning a grand slam?": [
+                "Marcelo Rios",
+                "Roger Federer",
+                "Stefan Edberg",
+                "Arthur Ashe"
+            ]
+        },
+        {
+            "Who was the first men\'s tennis player to win a single grand slam ten times?": [
+                "Rafael Nadal",
+                "Roger Federer",
+                "Pete Sampras",
+                "Rod Laver"
+            ]
+        },
+        {
+            "What horse won the 2017 Kentucky Derby?": [
+                "Always Dreaming",
+                "Lookin At Lee",
+                "Classic Empire",
+                "Battle of Midway"
+            ]
+        },
+        {
+            "Which NHL team won the 2017 Stanley Cup finals against the Nashville Predators?": [
+                "Pittsburgh Penguins",
+                "San Jose Sharks",
+                "Columbus Blue Jackets",
+                "Washington Capitals"
+            ]
+        },
+        {
+            "What NFL team won Super Bowl 51 in overtime?": [
+                "New England Patriots",
+                "Dallas Cowboys",
+                "San Francisco 49ers",
+                "Atlanta Falcons"
+            ]
+        },
+        {
+            "Who was the NBA Most Valuable Player for the 2016-2017 season?": [
+                "Russell Westbrook",
+                "Draymond Green",
+                "Kawhi Leonard",
+                "James Harden"
+            ]
+        },
+        {
+            "Going to the Cleveland Browns, who was the first pick in the 2017 NFL draft?": [
+                "Myles Garrett",
+                "Leonard Fournette",
+                "Solomon Thomas",
+                "Mitchell Trubisky"
+            ]
+        },
+        {
+            "Held in Bermuda, the 2017 Louis Vuitton Cup was awarded to Team New Zealand in which sport?": [
+                "Sailing",
+                "Rugby Sevens",
+                "Tennis",
+                "Cricket"
+            ]
+        },
+        {
+            "Held in June 2017, who won the 2017 U.S. Open Golf Championship?": [
+                "Brooks Koepka",
+                "Adam Scott",
+                "Jordan Speith",
+                "Rory McIlroy"
+            ]
+        },
+        {
+            "Captain for the Pittsburgh Penguins, what NHL star won his second Maurice Richard Trophy as leading goal scorer in 2017?": [
+                "Sidney Crosby",
+                "Patrick Kane",
+                "Brent Burns",
+                "Alex Ovechkin"
+            ]
+        },
+        {
+            "The 2017 Sudirman Cup was awarded to Team South Korea in what sport on May 28th, 2017?": [
+                "Badminton",
+                "Karate",
+                "Volleyball",
+                "Table Tennis"
+            ]
+        },
+        {
+            "Who won the 2017 Associated Press NFL Most Valuable Player Award?": [
+                "Matt Ryan",
+                "Dak Prescott",
+                "Odell Beckham Jr.",
+                "Tom Brady"
+            ]
+        },
+        {
+            "Taking place in Edinburgh, which team won the 2017 Rugby Union European Cup?": [
+                "Saracens",
+                "Leicester Tigers",
+                "Sale Sharks",
+                "Exeter Chiefs"
+            ]
+        },
+        {
+            "One year after blowing a three to one lead to the Cleveland Cavaliers in the NBA Finals, this team came back in 2017 to win the NBA championship in dominating fashion.": [
+                "Golden State Warriors",
+                "Cleveland Cavaliers",
+                "Boston Celtics",
+                "Los Angeles Lakers"
+            ]
+        },
+        {
+            "Which men\'s tennis player holds the Open Era record for most consecutive clay court victories with?": [
+                "Rafael Nadal",
+                "Roger Federer",
+                "Guillermo Vilas",
+                "Carlos Moya"
+            ]
+        },
+        {
+            "Who was the first pick of the 2017 NBA draft?": [
+                "Markelle Fultz",
+                "Jayson Tatum",
+                "Lonzo Ball",
+                "Josh Jackson"
+            ]
+        },
+        {
+            "In what month did the 2017 Tour de France take place?": [
+                "July",
+                "June",
+                "May",
+                "April"
+            ]
+        },
+        {
+            "Who holds the NBA record for most points scored in a one quarter": [
+                "Klay Thompson",
+                "Kobe Bryant",
+                "Michael Jordan",
+                "Stephen Curry"
+            ]
+        },
+        {
+            "Who is the only men\'s professional boxer in history to maintain a perfect record at 49 wins to 0 losses": [
+                "Floyd Mayweather",
+                "Muhammad Ali",
+                "Sugar Ray Leonard",
+                "Mike Tyson"
+            ]
+        },
+        {
+            "Set in 1962, what basketball star holds the NBA record for most points in a single game?": [
+                "Wilt Chamberlin",
+                "Shaquille O\'Neal",
+                "Michael Jordan",
+                "Oscar Robertson"
+            ]
+        },
+        {
+            "Known as The King, what professional driver ended his career with a record 200 NASCAR wins?": [
+                "Richard Petty",
+                "Dale Earnhardt",
+                "Jeff Gordon",
+                "Jimmie Johnson"
+            ]
+        },
+        {
+            "Which of these events is not one of the four tennis Grand Slam tournaments": [
+                "Davis Cup",
+                "Wimbledon",
+                "French Open",
+                "Australian Open"
+            ]
+        },
+        {
+            "What is the distance between the wickets in a game of cricket?": [
+                "22 yards",
+                "26 yards",
+                "18 yards",
+                "32 yards"
+            ]
+        },
+        {
+            "What color jersey is awarded to the race leader in the Tour de France?": [
+                "Yellow",
+                "Blue",
+                "Red",
+                "Green"
+            ]
+        },
+        {
+            "Who was the first skateboarder to land the 900 aerial at the 1999 X Games?": [
+                "Tony Hawk",
+                "Ryan Sheckler",
+                "Stefen Janoski",
+                "Rob Dyrdek"
+            ]
+        },
+        {
+            "Which team won an unprecedented three World Series titles in five years from 2010 to 2014?": [
+                "San Francisco Giants",
+                "Boston Red Sox",
+                "New York Yankees",
+                "Los Angeles Dodgers"
+            ]
+        },
+        {
+            "Who was the first African American woman to reach number one in the WTA tennis rankings?": [
+                "Venus Williams",
+                "Serena Williams",
+                "Althea Gibson",
+                "Madison Keys"
+            ]
+        },
+        {
+            "In what year did the first ever Wimbledon Tennis Championship take place?": [
+                "1907",
+                "1967",
+                "1877",
+                "1957"
+            ]
+        },
+        {
+            "Who was the first athlete to earn All-Star Game nominations in two sports (baseball and football)?": [
+                "Deion Sanders",
+                "Bo Jackson",
+                "Lebron James",
+                "Tim Tebow"
+            ]
+        },
+        {
+            "How many track and field events are featured in an Olympic Heptathlon?": [
+                "7",
+                "4",
+                "10",
+                "6"
+            ]
+        },
+        {
+            "Which of the following U.S. states has four NBA teams?": [
+                "California",
+                "Florida",
+                "New York",
+                "Texas"
+            ]
+        },
+        {
+            "Who won the first ever World Cup?": [
+                "Uruguay",
+                "Brazil",
+                "Italy",
+                "Germany"
+            ]
+        },
+        {
+            "How many times have Germany won the World Cup?": [
+                "4",
+                "3",
+                "1",
+                "2"
+            ]
+        },
+        {
+            "Who is England\'s all time leading scorer in World Cup history?": [
+                "Gary Lineker",
+                "David Beckham",
+                "Geoff Hurst",
+                "Bobby Charlton"
+            ]
+        },
+        {
+            "What was the score in penalties at the 1994 World Cup final in the USA?": [
+                "3 to 1",
+                "3 to 2",
+                "5 to 4",
+                "5 to 3"
+            ]
+        },
+        {
+            "Which men\'s soccer player has won the most FIFA Ballons d\'Or?": [
+                "Lionel Messi",
+                "Cristiano Ronaldo",
+                "Pele",
+                "Neymar"
+            ]
+        },
+        {
+            "Which men\'s soccer player has scored the most UEFA club competition goals?": [
+                "Cristiano Ronaldo",
+                "Lionel Messi",
+                "Zlatan Ibrahimovic",
+                "Ronaldhiño"
+            ]
+        },
+        {
+            "Which women\'s tennis player holds the record for most Grand Slams won in the Open Era?": [
+                "Serena Williams",
+                "Venus Williams",
+                "Steffi Graf",
+                "Martina Hingis"
+            ]
+        },
+        {
+            "Which men\s tennis player holds the record for most number of weeks ranked number one in the ATP rankings? ": [
+                "Roger Federer",
+                "Pete Sampras",
+                "Rod Laver",
+                "Novak Djokovic"
+            ]
+        },
+        {
+            "Who is the only men\'s tennis player to win the calendar year grand slam?": [
+                "Rod Laver",
+                "Roger Federer",
+                "Rafael Nadal",
+                "Novak Djokovic"
+            ]
+        },
+        {
+            "Who is the only women\'s tennis player to win the calendar year golden slam?": [
+                "Steffi Graf",
+                "Serena Williams",
+                "Martina Navratilova",
+                "Monica Seles"
+            ]
+        },
+        {
+            "Which women\'s tennis player famously beat Bobby Riggs in the Battle of the Sexes?": [
+                "Billie Jean King",
+                "Serena Williams",
+                "Steffi Graf",
+                "Martina Hingis"
+            ]
+        },
+        {
+            "In 1972, what NBA team set a record by winning 33 straight games and won 69 in all during the regular season?": [
+                "Los Angeles Lakers",
+                "Boston Celtics",
+                "Detroit Pistons",
+                "Miami Heat"
+            ]
+        },
+        {
+            "Which NBA player famously fought through the flu to play in game 5 of the 1997 NBA finals, dropping 38 points?": [
+                "Michael Jordan",
+                "Charles Barkley",
+                "Hakeem Olajuon",
+                "Reggie Miller"
+            ]
+        },
+        {
+            "Which NBA player holds the record for most number of three pointers made in an NBA season, with 402 in 2015-2016?": [
+                "Stephen Curry",
+                "Reggie Miller",
+                "Ray Allen",
+                "Kyle Korver"
+            ]
+        },
+        {
+            "Who is the only NBA player to record a triple double without points?": [
+                "Draymond Green",
+                "Oscar Robertson",
+                "Russell Westbrook",
+                "Rajon Rondo"
+            ]
+        },
+        {
+            "Which NBA player earned the nickname The Admiral?": [
+                "David Robinson",
+                "Bill Russell",
+                "Tim Duncan",
+                "Karl Malone"
+            ]
+        },
+        {
+            "Playing for the Los Angeles Sparks, who was the first woman to dunk in a WNBA game?": [
+                "Lisa Leslie",
+                "Sue Bird",
+                "Britney Griner",
+                "Cynthia Cooper"
+            ]
+        },
+        {
+            "Which college men\'s basketball team has the most number of final four appearances?": [
+                "North Carolina",
+                "Duke",
+                "Kansas",
+                "Kentucky"
+            ]
+        },
+        {
+            "Which college men\'s basketball team has the most number of national titles?": [
+                "UCLA",
+                "Duke",
+                "North Carolina",
+                "Kentucky"
+            ]
+        },
+        {
+            "Which player holds the record for most points scored against a NCAA Division One men\'s basketball opponent, with 72 in 1991?": [
+                "Kevin Bradshaw",
+                "Kyrie Irving",
+                "Pete Maravich",
+                "Lew Alcindor"
+            ]
+        },
+        {
+            "Who has the most wins as a head coach in the NFL?": [
+                "Don Shula",
+                "Bill Belichick",
+                "Curly Lambeau",
+                "Vince Lombardi"
+            ]
+        },
+        {
+            "Who is the last non-quarterback to win NFL MVP?": [
+                "Adrian Peterson",
+                "Ray Lewis",
+                "Shaun Alexander",
+                "Marshawn Lynch"
+            ]
+        },
+        {
+            "Who is the only freshman to win the Heisman trophy, in 2012?": [
+                "Johnny Manziel",
+                "Vince Young",
+                "Tim Tebow",
+                "Cam Newton"
+            ]
+        },
+        {
+            "How mant Heiman Trophy winners have gone on to be MVP of the Super Bowl?": [
+                "4",
+                "3",
+                "2",
+                "1"
+            ]
+        },
+        {
+            "Which NFL player holds the record for most career receiving yards?": [
+                "Jerry Rice",
+                "Randy Moss",
+                "Terrell Owens",
+                "Calvin Johnson"
+            ]
+        },
+        {
+            "Which NBA player holds the record for most rebounds in a single game with 50?": [
+                "Wilt Chamberlin",
+                "Dwight Howard",
+                "Dennis Rodman",
+                "Draymond Green"
+            ]
+        },
+        {
+            "Which golfer holds the record for lowest scoring average in a single season, with 67.79?": [
+                "Tiger Woods",
+                "Jack Nicklaus",
+                "Arnold Palmer",
+                "Phil Mickelson"
+            ]
+        },
+        {
+            "Who was the first billionaire athlete?": [
+                "Michael Jordan",
+                "Tiger Woods",
+                "Cristiano Ronaldo",
+                "Lebron James"
+            ]
+        },
+        {
+            "Which MLB player holds the record for lowest ERA in a single season, with 0.86?": [
+                "Tim Keefe",
+                "Sandy Koufax",
+                "Willie McCovey",
+                "Curt Schilling"
+            ]
+        },
+        {
+            "Which MLB player holds the record for most career home runs, with 762": [
+                "Barry Bonds",
+                "Hank Arron",
+                "Babe Ruth",
+                "Jackie Robinson"
+            ]
+        },
+        {
+            "How many gold medals did Michael Phelps win during his historic 2008 Beijing Olympics campaign?": [
+                "8",
+                "5",
+                "10",
+                "12"
+            ]
+        },
+        {
+            "Who was the first women\'s gymnast to earn a perfect score of 10.0 in the 1976 Montreal Olympics": [
+                "Nadia Comaneci",
+                "Olga Korbut",
+                "McKayla Maroney",
+                "Simone Biles"
+            ]
+        },
+        {
+            "Which player dunked all over the French Men\'s National Basketball Team in 2000, widely regarded as the best dunk of all time?": [
+                "Vince Carter",
+                "Lebron James",
+                "Kevin Garnett",
+                "Tracy McGrady"
+            ]
+        },
+        {
+            "Which NBA player earned the nickname The King?": [
+                "Lebron James",
+                "Kobe Bryant",
+                "Michael Jordan",
+                "Larry Bird"
+            ]
+        },
+        {
+            "Which NBA player famously burst on the scene by crossing up Michael Jordan during his rookie season?": [
+                "Allen Iverson",
+                "Isiah Thomas",
+                "Kobe Bryant",
+                "Charles Barkley"
+            ]
+        },
+        {
+            "Which men\'s tennis player earned fame with the tag line Image is Everything ": [
+                "Andre Agassi",
+                "Pete Sampras",
+                "John McEnroe",
+                "Jim Courier"
+            ]
+        }
+    ]
+};
